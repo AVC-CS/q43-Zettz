81fef1c1db5262f51aed44e800c04eb9fee8c001
Kyu Lee <kwooler@gmail.com>
2026-03-05 13:35:20 -0800

    Student starter code

--- main.cpp
+++ main.cpp
@@ -13,19 +13,6 @@ int main()
     // TODO
     // Find max, min, mid using only if-statements (no loops, no functions, no arrays)
 
-    // Find max
-    maxVal = num1;
-    if (num2 > maxVal) maxVal = num2;
-    if (num3 > maxVal) maxVal = num3;
-
-    // Find min
-    minVal = num1;
-    if (num2 < minVal) minVal = num2;
-    if (num3 < minVal) minVal = num3;
-
-    // Find mid (the one that is not max and not min)
-    midVal = num1 + num2 + num3 - maxVal - minVal;
-
     cout << maxVal << " " << midVal << " " << minVal << endl;
 
     return 0;
