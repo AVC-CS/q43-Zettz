497fcfb81d12202a54875b64cf9d636f6ce77fb3
Lorenzo <calderonlorenzo026@gmail.com>
2026-03-20 00:48:05 +0000

    q43 code completed

--- main.cpp
+++ main.cpp
@@ -12,6 +12,34 @@ int main()
 
     // TODO
     // Find max, min, mid using only if-statements (no loops, no functions, no arrays)
+    if (num1 >= num2 && num1 >= num3) {
+        maxVal = num1;
+        if (num2 >= num3) {
+            midVal = num2;
+            minVal = num3;
+        } else {
+            midVal = num3;
+            minVal = num2;
+        }
+    } else if (num2 >= num1 && num2 >= num3) {
+        maxVal = num2;
+        if (num1 >= num3) {
+            midVal = num1;
+            minVal = num3;
+        } else {
+            midVal = num3;
+            minVal = num1;
+        }
+    } else {
+        maxVal = num3;
+        if (num1 >= num2) {
+            midVal = num1;
+            minVal = num2;
+        } else {
+            midVal = num2;
+            minVal = num1;
+        }
+    }
 
     cout << maxVal << " " << midVal << " " << minVal << endl;
 
